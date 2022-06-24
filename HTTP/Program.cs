316f1579540fcbc81794ac7717775d40ddcaf2f8
WellingtonDeveloper <wellington.developer.caldeira@gmail.com>
2022-06-24 18:10:59 -0300

    Requisição de todas as tarefas ou busca especifica de penas uma tarefa informada pelo usuário.

--- HTTP/Program.cs
+++ HTTP/Program.cs
@@ -10,34 +10,101 @@ namespace HTTP
 {
     class Program
     {
+        enum Menu { ListarTudo = 1, ListarUnica, Sair}
         static void Main(string[] args)
         {
-            //cadastro da requisição
-            WebRequest request = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/");
-            request.Method = "GET";
-            var resposta = request.GetResponse(); // recebe a resposta da resquisição
-
-            using (resposta)//a a resposta da requisição dentro da estrututa Using que vai processar a resposta
+            bool escolheuSair = false;
+            while (escolheuSair == false)
             {
-                var stream = resposta.GetResponseStream();//Peguei o resultado da resposta e coloquei na vairavel stream
-                StreamReader leitor = new StreamReader(stream);//Coloquei a resposta dentro de um leitor
-                object dados = leitor.ReadToEnd();//Li a resposta e transformei em uma string e passsei para a variavel dados
+                Console.WriteLine("Sistema de Request");
+                Console.WriteLine("1 - Listar todas as tarefas\n2 - Buscar tarefa especifica\n3 - Sair");
+                string opStr = Console.ReadLine();
+                int opInt = int.Parse(opStr);
 
-                //Console.WriteLine(dados.ToString());//Exibindo no Console
-                //Estamos recebendo um tipo List, se colocarmos apenas como uma variavel do tipo tarefas.
-                List<Tarefa> tarefas = JsonConvert.DeserializeObject<List<Tarefa>>(dados.ToString());
-
-                foreach(Tarefa tarefa in tarefas)
+                if(opInt > 0 && opInt < 4)
                 {
-                    tarefa.Exibir();
+                    Menu escolha = (Menu)opInt;
+                    switch (escolha)
+                    {
+                        case Menu.ListarTudo:
+                            ReqList();
+                            break;
+                        case Menu.ListarUnica:
+                            ReqUnica();
+                            break;
+                        case Menu.Sair:
+                            escolheuSair = true;
+                            break;
+                    }
                 }
-                //Console.WriteLine(tarefas);
+                else
+                {
+                    escolheuSair = true;
+                }
+            }
+            //cadastro da requisição, retorna a lista de requisições
+            static void ReqList()
+            {
+                //Lista requisição.
+                WebRequest request = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/");
+                request.Method = "GET";
+                var resposta = request.GetResponse(); // recebe a resposta da resquisição
 
-                stream.Close();
-                resposta.Close();
+                using (resposta)//a a resposta da requisição dentro da estrututa Using que vai processar a resposta
+                {
+                    var stream = resposta.GetResponseStream();//Peguei o resultado da resposta e coloquei na vairavel stream
+                    StreamReader leitor = new StreamReader(stream);//Coloquei a resposta dentro de um leitor
+                    object dados = leitor.ReadToEnd();//Li a resposta e transformei em uma string e passsei para a variavel dados
+
+                    //Console.WriteLine(dados.ToString());//Exibindo no Console
+                    //Estamos recebendo um tipo List, se colocarmos apenas como uma variavel do tipo tarefas.
+                    List<Tarefa> tarefas = JsonConvert.DeserializeObject<List<Tarefa>>(dados.ToString());
+
+                    foreach (Tarefa tarefa in tarefas)
+                    {
+                        tarefa.Exibir();
+                    }
+                    //Console.WriteLine(tarefas);
+                    stream.Close();
+                    resposta.Close();
+                }
             }
 
-            Console.ReadLine();
-        }
+            //Requisição de uma unica tarefa
+            static void ReqUnica()
+            {
+                Console.WriteLine("Listar tarefa");
+                Console.WriteLine("\nDigite o ID da tarefa que deseja localizar:");
+                int idTarefa = int.Parse(Console.ReadLine());
+
+                if (idTarefa > 0 && idTarefa <= 200)
+                {
+
+                    WebRequest request = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/" + idTarefa);//conctenação no link +id"String id Console.ReadLine();
+                    request.Method = "GET";
+                    var resposta = request.GetResponse(); // recebe a resposta da resquisição
+
+                    using (resposta)//a a resposta da requisição dentro da estrututa Using que vai processar a resposta
+                    {
+                        var stream = resposta.GetResponseStream();//Peguei o resultado da resposta e coloquei na vairavel stream
+                        StreamReader leitor = new StreamReader(stream);//Coloquei a resposta dentro de um leitor
+                        object dados = leitor.ReadToEnd();//Li a resposta e transformei em uma string e passsei para a variavel dados
+
+                        //Console.WriteLine(dados.ToString());//Exibindo no Console
+                        //Estamos recebendo uma tarefa especifica.
+                        Tarefa tarefa = JsonConvert.DeserializeObject<Tarefa>(dados.ToString());
+                        tarefa.Exibir();
+
+                        //Console.WriteLine(tarefa);
+                        stream.Close();
+                        resposta.Close();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Id existente.");
+                }
+            }
+       }
     }
 }
