0523015d65abb356f12088ccde8499356fda7f09
WellingtonDeveloper <wellington.developer.caldeira@gmail.com>
2022-06-24 11:32:14 -0300

    Neste primeiro momento vamos utilizar a biblioteca Newtonsoft.Jason que é usada para converter Json para C#, para isso vamos precisar de um repositorio de bibliotecas, o Nugets. No Nuget tem uma integração completa com o VS, com vários pacotes, no caso vamos usar apenas para instalar a biblioteca Newtonsoft.Jason.

--- HTTP/Program.cs
+++ HTTP/Program.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace HTTP
 {
@@ -10,16 +11,18 @@ namespace HTTP
     {
         static void Main(string[] args)
         {
+            //cadastro da requisição
             WebRequest request = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/");
             request.Method = "GET";
+            var resposta = request.GetResponse(); // recebe a resposta da resquisição
 
-            using (var resposta = request.GetResponse())
+            using (resposta)//a a resposta da requisição dentro da estrututa Using que vai processar a resposta
             {
-                var stream = resposta.GetResponseStream();
-                StreamReader leitor = new StreamReader(stream);
-                object dados = leitor.ReadToEnd();
+                var stream = resposta.GetResponseStream();//Peguei o resultado da resposta e coloquei na vairavel stream
+                StreamReader leitor = new StreamReader(stream);//Coloquei a resposta dentro de um leitor
+                object dados = leitor.ReadToEnd();//Li a resposta e transformei em uma string e passsei para a variavel dados
 
-                Console.WriteLine(dados.ToString());
+                Console.WriteLine(dados.ToString());//Exibindo no Console
 
                 stream.Close();
                 resposta.Close();
