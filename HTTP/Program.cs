0994ed39d000b4ef0bc85eaf2aa8598be94bb963
WellingtonDeveloper <wellington.developer.caldeira@gmail.com>
2022-06-24 10:24:47 -0300

    Add project files.

---
+++ HTTP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest request = WebRequest.Create("https://jsonplaceholder.typicode.com/todos/");
+            request.Method = "GET";
+
+            using (var resposta = request.GetResponse())
+            {
+                var stream = resposta.GetResponseStream();
+                StreamReader leitor = new StreamReader(stream);
+                object dados = leitor.ReadToEnd();
+
+                Console.WriteLine(dados.ToString());
+
+                stream.Close();
+                resposta.Close();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
